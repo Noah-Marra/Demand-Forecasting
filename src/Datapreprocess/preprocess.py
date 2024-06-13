27cacaea75dfa1f54e91d6aea08b6596f248fd18
Noah-Marra <nmarra19@gmail.com>
2024-06-12 20:51:18 -0400

    EDA and Data preprocessing scripts

---
+++ src/Datapreprocess/preprocess.py
@@ -0,0 +1,12 @@
+import pandas as pd
+def preprocess(df):
+    """
+    Preprocess the data removing uncessary data and setting elements to their correct types
+    Handle Null values by backward fill null values
+    """
+    df.drop(['Unnamed: 0'], axis=1, inplace=True)#remove empty column
+    df.drop(['type'], axis=1, inplace=True) #remove type column as highly correlated with size column. Size has more detail
+    df['Date'] = pd.to_datetime(df['Date']) #update column from string to datetime
+    df.set_index('Date', inplace=True) 
+    df.fillna(method='bfill', inplace=True)
+    return df
